aefb3b064b3f47cb12fe55cd779cfdf485b71805
Simon Sippert <s.sippert@techdivision.com>
2019-02-05 11:55:15 +0100

    Initial commit

---
+++ Api/Constants.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ */
+
+namespace Magenerds\WysiwygWidget\Api;
+
+/**
+ * Class Constants
+ *
+ * @package     Magenerds\WysiwygWidget
+ * @file        Constants.php
+ * @copyright   Copyright (c) 2019 TechDivision GmbH (https://www.techdivision.com)
+ * @site        https://www.techdivision.com/
+ * @author      Simon Sippert <s.sippert@techdivision.com>
+ */
+interface Constants
+{
+    /**
+     * This prefix gets added to base64 encoded content
+     *
+     * @type string
+     */
+    const BASE64_PREFIX = '---BASE64---';
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Magenerds Wysiwyg Widget
+
+This module adds a wysiwyg editor to the widget menu.
